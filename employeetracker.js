178e6d1ec2823e7b243fa30b1758780888febe6c
Jack Meier <jackson.meier423@gmail.com>
2020-07-16 23:46:37 -0700

    added create and add view employee feature

--- employeetracker.js
+++ employeetracker.js
@@ -157,10 +157,106 @@ function addRole() {
 }
 
 // Views Roles
-// Views Departments
 function viewRole() {
   const deptSql = `SELECT * FROM role`;
   connection.query(deptSql, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    console.table(results);
+    mainMenu();
+  });
+}
+
+function addEmployee() {
+  const roleSql = `SELECT * FROM role;`;
+  connection.query(roleSql, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    let roles = [];
+    for (i = 0; i < results.length; i++) {
+      roles.push(results[i]);
+    }
+    inquirer
+      .prompt([
+        {
+          name: "firstName",
+          type: "input",
+          message: "What is the fist name of the employee?",
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "What is the last name of the employee?",
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is the employee's role?",
+          choices: roles.map((u) => {
+            return {
+              name: u.title,
+              value: u.id,
+            };
+          }),
+        },
+      ])
+      .then(function (nameChoices) {
+        const managerSql = `SELECT first_name FROM employee;`;
+        connection.query(managerSql, (error, results) => {
+          if (error) {
+            throw error;
+          }
+          let managers = [];
+          for (i = 0; i < results.length; i++) {
+            managers.push(results[i]);
+          }
+          inquirer
+            .prompt({
+              name: "managerName",
+              type: "list",
+              message: "Who is this employee's manager?",
+              choices: managers.map((u) => {
+                return {
+                  name: u.first_name,
+                  value: u.id,
+                };
+              }),
+            })
+            .then(function (managerChoice) {
+              const query = connection.query(
+                "INSERT INTO employee(first_name, last_name, role_id) VALUES (?, ?, ?),",
+                [
+                  nameChoices.firstName,
+                  nameChoices.lastName,
+                  parseInt(nameChoices.role),
+                ],
+                (error, results) => {
+                  console.log("=================");
+                  console.log(query);
+                  console.log("=================");
+
+                  mainMenu();
+                }
+              );
+            });
+        });
+      });
+  });
+}
+
+function viewEmployee() {
+  const employeeSql = `SELECT employee.id AS ID, 
+	CONCAT(employee.first_name, " ", employee.last_name) AS Name,
+	role.title AS Title,
+	department.name AS Department,
+	role.salary AS Salary
+
+FROM employee
+    INNER JOIN role ON employee.role_id = role.id
+    INNER JOIN department ON role.department_id = department.id;`;
+  connection.query(employeeSql, (error, results) => {
     if (error) {
       throw error;
     }
